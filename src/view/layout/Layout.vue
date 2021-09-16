77d4854c1de80c502689c960e5e006dd92ad4fc0
simao234430 <simao234430@gmail.com>
2021-09-16 10:31:52 +0800

    add router

---
+++ src/view/layout/Layout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex flex-column flex-root">2423</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {},
+};
+</script>
+<style lang="sass" scoped>
+//@import url(); 引入公共css类
+</style>
---
+++ src/view/pages/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="">2233</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {},
+};
+</script>
+<style lang="sass" scoped>
+//@import url(); 引入公共css类
+</style>
